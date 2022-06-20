c67da5ae7d9c32a2d424912060066b06c149d714
Patrick Young <patrickyoung281@gmail.com>
2022-06-20 09:48:06 -0400

    Code Cleaned Up

--- script.js
+++ script.js
@@ -1,31 +1,30 @@
+//fetch data from public API
+
 fetch("https://picsum.photos/v2/list")
     .then((resp) => resp.json())
     .then(function (data) {
         addFirstFlashCard(data)
     });
 
-
 function addFirstFlashCard(jsonList) {
 
-
-    const firstTwoCards = jsonList.slice(2,4)
-    firstTwoCards[0]["text"] = "Wenn ich Freizeit habe, \n gehe ich gerne wandern." 
+    const firstTwoCards = jsonList.slice(2,4)   //create new array of objects from original API data
+    firstTwoCards[0]["text"] = "Wenn ich Freizeit habe, \n gehe ich gerne wandern."     //add new key: value pairs to each of the two objects in sliced array
     firstTwoCards[0]["English"] = "to hike"
     firstTwoCards[0]["GermanVerb"] = "wandern"
     firstTwoCards[1]["text"] = "Wenn ich Freizeit habe, \n gehe ich gerne schwimmen." 
     firstTwoCards[1]["English"] = "to swim"
     firstTwoCards[1]["GermanVerb"] = "schwimmen"
 
-    firstTwoCards.forEach(function (element, i) {
+    firstTwoCards.forEach(function (element, i) {   //iterate over my sliced array using forEach
 
-    const container = document.getElementById("cardContainer")
+    const container = document.getElementById("cardContainer") //create new DOM elements for each array element. This is where API data will be displayed for the user
     const newDiv = document.createElement("div")
     newDiv.className = "cards"
     container.appendChild(newDiv)
-    const firstCardImage = element.download_url
-
-    const imageTag = document.createElement("img")
-    imageTag.src = firstCardImage
+    
+    const imageTag = document.createElement("img") // create img element for the image from the API
+    imageTag.src = element.download_url // the image URL from the API becomes the src for the image tag
     imageTag.className = "image"
     imageTag.style.padding = "5px";
     imageTag.style.borderStyle = "solid";
@@ -33,20 +32,15 @@ function addFirstFlashCard(jsonList) {
     imageTag.style.borderWidth = "5px";
     newDiv.appendChild(imageTag); 
 
-
-    const textTag = document.createElement("h2")
+    const textTag = document.createElement("h2") // create text element for each image 
     textTag.id = `Card${i}`
-
-    textTag.innerText = element.text 
+    textTag.innerText = element.text // text accessed from the key:value pair created on lines 12 and 15 
     textTag.className = "frontText"
     textTag.style.padding = "40px";
     textTag.style.lineHeight = "40px";
     newDiv.appendChild(textTag);
 
-    
-    
-    
-    const button = document.createElement("button")
+    const button = document.createElement("button") //creates the English translation button 
     button.type = "button" 
     button.innerText = "Click here for English translation" 
     button.className = "firstButtons"
@@ -55,10 +49,15 @@ function addFirstFlashCard(jsonList) {
     button.style.bottom = "115px";
     button.style.left = "30px";
     newDiv.appendChild(button)
-    const english = element.English
     button.addEventListener("click", showEnglishTranslation);
 
-    const gotItButton = document.createElement("button")
+    const english = element.English //creates global english variable for use in two following functions 
+
+    function showEnglishTranslation() { 
+        alert(`English translation: "When I have free time, I like ${english}."`);
+    }
+
+    const gotItButton = document.createElement("button") //creates got it button
     gotItButton.className = "gotItButtons0"
     newDiv.appendChild(gotItButton)
     gotItButton.innerText = "Click here when you feel like you have memorized the word!" 
@@ -66,45 +65,38 @@ function addFirstFlashCard(jsonList) {
 
     gotItButton.addEventListener("click", changeColor);
 
-   function changeColor () {
+    function changeColor () {
         gotItButton.style.backgroundColor = "MediumSpringGreen";
         gotItButton.fontSize = "24px"; 
         gotItButton.innerText = `Gratuliere! You know that "${element.GermanVerb}" means "${english}."\nKeep going until you have memorized all of the words!`;
-
     } 
-
-    function showEnglishTranslation() {
-        alert(`English translation: "When I have free time, I like ${english}."`);
-    }
-    })
-
-
-    
+    })    
 }
 
-const form = document.querySelector("form");
+const form = document.querySelector("form"); //user created flashcards form 
 
-form.addEventListener("submit", addNewCard)
+form.addEventListener("submit", addNewCard) //fired when a user submits a new card 
 
 function addNewCard(event) {
     event.preventDefault()
-    const name = event.target.name.value
+    const name = event.target.name.value // gives access to three form input boxes/values 
     const image = event.target.image.value 
     const english = event.target.englishTranslation.value
 
-    const container = document.getElementById("cardContainer")
+    const container = document.getElementById("cardContainer") //new card DOM div
     const newDiv = document.createElement("div")
     newDiv.className = "cards"
     container.appendChild(newDiv) 
-    const text = document.createElement("h2")
+
+
+    const text = document.createElement("h2") //new card German text 
     const userImage = document.createElement("img")
     text.innerText = `Wenn ich Freizeit habe, \n gehe ich gerne ${name}`
     text.style.padding = "40px";
     text.style.lineHeight = "40px";
-    
-    
-    userImage.src = image 
     text.className = "frontText"
+    
+    userImage.src = image //new card image 
     userImage.className = "image" 
     userImage.style.padding = "5px";
     userImage.style.borderStyle = "solid";
@@ -113,7 +105,7 @@ function addNewCard(event) {
     newDiv.appendChild(userImage)
     newDiv.appendChild(text)
     
-    const button = document.createElement("button")
+    const button = document.createElement("button") //new card English translation button for user-created cards  
     button.type = "button" 
     button.innerText = "Click here for English translation" 
     button.className = "newButtons"
@@ -124,38 +116,23 @@ function addNewCard(event) {
     button.style.left = "30px";
     button.addEventListener("click", showEnglishTranslation)
 
-
-    const englishElement = document.createElement("h3")
-    englishElement.className = "englishTranslation"
-    englishElement.innerText = `(${name} = ${english})` 
-    newDiv.appendChild(englishElement)
-
-    newDiv.setAttribute("id", name) 
-
-    
-    const buttons = document.querySelector("button")
-
-    console.log("buttons", buttons)
-
-    buttons.addEventListener("click", showEnglishTranslation);
-
     function showEnglishTranslation() {
         alert(`English translation: "When I have free time, I like ${english}."`);
     }
 
-    const gotItButton = document.createElement("button")
+    const gotItButton = document.createElement("button") //creates got it button for user-created cards 
     gotItButton.className = "gotItButtons"
     newDiv.appendChild(gotItButton)
     gotItButton.innerText = "Click here when you feel like you have memorized the word!" 
     gotItButton.style.backgroundColor = "yellow";
     gotItButton.style.position = "relative";
-    gotItButton.style.left = "340px";
-    gotItButton.style.bottom = "120px";
+    gotItButton.style.left = "-600px";
+    gotItButton.style.bottom = "50px";
     gotItButton.style.fontSize = "16px";
 
     gotItButton.addEventListener("click", changeColor);
 
-   function changeColor () {
+    function changeColor () {
         gotItButton.style.backgroundColor = "MediumSpringGreen";
         gotItButton.fontSize = "24px"; 
         gotItButton.innerText = `Gratuliere! You know that "${name}" means "${english}."\nKeep going until you have memorized all of the words!`;
